06c2553bbdc39909c927482f270c8b09c16575dc
Iris0908 <wangming_ch@163.com>
2018-12-04 18:09:35 +0800

    u

--- DS/_239b6711c9a70f442e40e1a1c2261912_Programming-Assignment-2/job_queue/job_queue.py
+++ DS/_239b6711c9a70f442e40e1a1c2261912_Programming-Assignment-2/job_queue/job_queue.py
@@ -1,4 +1,5 @@
 # python3
+import time
 
 class JobQueue:
     def read_data(self):
@@ -6,7 +7,29 @@ class JobQueue:
         self.jobs = list(map(int, input().split()))
         assert m == len(self.jobs)
 
+        '''
+        fh = open('DS/_239b6711c9a70f442e40e1a1c2261912_Programming-Assignment-2/job_queue/tests/08', 'r')
+        index = 0
+        for line in fh.readlines():
+            if index == 0:
+                self.num_workers, m = map(int, line.split())
+                index = index + 1
+                #print('size', buffer_size, 'number', n_requests)
+            else:
+                self.jobs = list(map(int, line.split()))
+        '''
+
     def write_response(self):
+        '''
+        fh = open('DS/_239b6711c9a70f442e40e1a1c2261912_Programming-Assignment-2/job_queue/tests/08.a', 'r')
+        index = 0
+        for line in fh.readlines():
+            swap_test_0, swap_test_1 = map(int, line.split())
+            if swap_test_0 != self.assigned_workers[index] or swap_test_1 != self.start_times[index]:
+                print('error', index+1, swap_test_0, swap_test_1, self.assigned_workers[index], self.start_times[index])
+                break
+            index = index + 1
+        '''
         for i in range(len(self.jobs)):
           print(self.assigned_workers[i], self.start_times[i]) 
 
@@ -14,19 +37,60 @@ class JobQueue:
         # TODO: replace this code with a faster algorithm.
         self.assigned_workers = [None] * len(self.jobs)
         self.start_times = [None] * len(self.jobs)
-        next_free_time = [0] * self.num_workers
+        #next_free_time = [[0]] * self.num_workers
+        next_free_time = []
+        for i in range(self.num_workers):
+            next_free_time.append([i, 0])
+        
+        root = 0
         for i in range(len(self.jobs)):
-          next_worker = 0
-          for j in range(self.num_workers):
-            if next_free_time[j] < next_free_time[next_worker]:
-              next_worker = j
-          self.assigned_workers[i] = next_worker
-          self.start_times[i] = next_free_time[next_worker]
-          next_free_time[next_worker] += self.jobs[i]
+            next_worker = root
+            self.assigned_workers[i] = next_free_time[next_worker][0]
+            self.start_times[i] = next_free_time[next_worker][1]
+            next_free_time[next_worker][1] += self.jobs[i]
+
+            #shift-down min-heap
+            min = root
+            node = root
+            while 2*node + 1 < self.num_workers:
+                if next_free_time[min][1] == next_free_time[2*node + 1][1]:
+                    if next_free_time[min][0] > next_free_time[2*node + 1][0]:
+                        min = 2*node + 1
+
+                #print('0000000:', node, min, self._data[min], self._data[2*node + 1])
+                if next_free_time[min][1] > next_free_time[2*node + 1][1]:
+                    min = 2*node + 1
+                    #print('qqqq:', node, 2*node + 1)
+        
+                if 2*node + 2 >= self.num_workers:
+                    if min != node:
+                        #self._swaps.append((node, min))
+                        #print('wwww, swap:', node, min, self._data[node], self._data[min])
+                        next_free_time[min], next_free_time[node] = next_free_time[node], next_free_time[min]
+                    break
+
+                if next_free_time[min][1] == next_free_time[2*node + 2][1]:
+                    if next_free_time[min][0] > next_free_time[2*node + 2][0]:
+                        min = 2*node + 2
+
+                if next_free_time[min][1] > next_free_time[2*node + 2][1]:
+                    min = 2*node + 2
+                    #print('eeee:', 2*node + 2)
+
+                if min != node:
+                    #self._swaps.append((node, min))
+                    #print('rrrr, swap:', node, min, self._data[node], self._data[min])          
+                    next_free_time[min], next_free_time[node] = next_free_time[node], next_free_time[min]
+                    node = min
+                else:
+                    #print('tttt:', node, min, self._data[node], self._data[2*node + 1], self._data[2*node + 2])
+                    break
 
     def solve(self):
         self.read_data()
+        #print(str(time.time()))
         self.assign_jobs()
+        #print(str(time.time()))
         self.write_response()
 
 if __name__ == '__main__':
--- DS/_239b6711c9a70f442e40e1a1c2261912_Programming-Assignment-2/make_heap/build_heap.py
+++ DS/_239b6711c9a70f442e40e1a1c2261912_Programming-Assignment-2/make_heap/build_heap.py
@@ -1,14 +1,15 @@
 # python3
-
+import time
 class HeapBuilder:
   def __init__(self):
     self._swaps = []
     self._data = []
 
   def ReadData(self):
-    #n = int(input())
-    #self._data = [int(s) for s in input().split()]
+    n = int(input())
+    self._data = [int(s) for s in input().split()]
     
+    '''
     fh = open('DS/_239b6711c9a70f442e40e1a1c2261912_Programming-Assignment-2/make_heap/tests/04', 'r')
     index = 0
     for line in fh.readlines():
@@ -17,14 +18,17 @@ class HeapBuilder:
             index = index + 1
             #print('size', buffer_size, 'number', n_requests)
         else:
-            self._data = line.split()
-    
-    #assert n == len(self._data)
+            #self._data = list(eval(str(line.split())))
+            self._data = [int(i) for i in line.split(" ")]
+    '''
+    assert n == len(self._data)
 
   def WriteResponse(self):
     print(len(self._swaps))
-    #for swap in self._swaps:
-      #print(swap[0], swap[1])
+    for swap in self._swaps:
+      print(swap[0], swap[1])
+    
+    '''
     fh = open('DS/_239b6711c9a70f442e40e1a1c2261912_Programming-Assignment-2/make_heap/tests/04.a', 'r')
     index = 0
     for line in fh.readlines():
@@ -39,7 +43,7 @@ class HeapBuilder:
                 print('error', index+1, swap_test_0, swap_test_1, swap_0, swap_1)
                 break
         index = index + 1
-
+    '''
 
   def GenerateSwaps(self):
     # The following naive implementation just sorts 
@@ -58,38 +62,42 @@ class HeapBuilder:
     '''
 
     lengh = len(self._data)
+    #print('data length:', lengh)
     for i in range(int(lengh/2) - 1, -1, -1):
       #print('rang:', i)
       min = i
       node = i
       while 2*node + 1 < lengh:
+        #print('0000000:', node, min, self._data[min], self._data[2*node + 1])
         if self._data[min] > self._data[2*node + 1]:
           min = 2*node + 1
-          #print('qqqq:', node)
+          #print('qqqq:', node, 2*node + 1)
         
         if 2*node + 2 >= lengh:
           if min != node:
             self._swaps.append((node, min))
+            #print('wwww, swap:', node, min, self._data[node], self._data[min])
             self._data[min], self._data[node] = self._data[node], self._data[min]
-            #print('wwww:', node)
           break
 
         if self._data[min] > self._data[2*node + 2]:
           min = 2*node + 2
-          #print('eeee:', node)
+          #print('eeee:', 2*node + 2)
 
         if min != node:
           self._swaps.append((node, min))
+          #print('rrrr, swap:', node, min, self._data[node], self._data[min])          
           self._data[min], self._data[node] = self._data[node], self._data[min]
           node = min
-          #print('rrrr:', node)
         else:
-          #print('tttt:', node)
+          #print('tttt:', node, min, self._data[node], self._data[2*node + 1], self._data[2*node + 2])
           break
 
   def Solve(self):
     self.ReadData()
+    #print(str(time.time()))
     self.GenerateSwaps()
+    #print(str(time.time()))
     self.WriteResponse()
 
 if __name__ == '__main__':
